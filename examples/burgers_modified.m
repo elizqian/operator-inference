d80366cb42d6708d32edc941f984c6c68442920a
smallpondtom <tkoike45@gmail.com>
2023-05-06 03:24:50 -0400

    make changeds

--- examples/burgers_modified.m
+++ examples/burgers_modified.m
@@ -26,7 +26,15 @@ mu = 0.1;               % diffusion coefficient
 
 % run FOM with input 1s to get reference trajectory
 u_ref = ones(K,1);
-[s_ref,A,B,F] = burgersFOM(N,dt,T_end,mu,u_ref);
+[s_ref,A,B,F,Ai] = burgersFOM(N,dt,T_end,mu,u_ref);
+
+%% Surface Plot for verification
+if true
+    s = surf(linspace(0.0,T_end,K+1),linspace(0.0,1.0,N),s_ref,'FaceAlpha',0.8);
+    s.EdgeColor = 'none';
+    xlabel("t");
+    ylabel("x");
+end
 
 %% Operator inference parameters
 params.modelform = 'LQI';           % model is linear-quadratic with input term
@@ -52,39 +60,38 @@ U = reshape(U_rand(:,1:num_inputs),K*num_inputs,1);
 [U_svd,s_svd,~] = svd(X,'econ'); % take SVD for POD basis
 
 %% for different basis sizes r, compute basis, learn model, and calculate state error 
-r_vals = 4:11;
+r_vals = 1:20;
 err_inf = zeros(length(r_vals),1);
 err_int = zeros(length(r_vals),1);  % MODIFIED
+
+% intrusive
+Vr = U_svd(:,1:max(r_vals));
+Aint = Vr' * A * Vr;
+Bint = Vr' * B;
+Ln = elimat(N); Dr = dupmat(max(r_vals));
+Fint = Vr' * F * Ln * kron(Vr,Vr) * Dr;
+
+% op-inf
+[operators] = inferOperators(X, U, Vr, params, R);
+Ahat = operators.A;
+Fhat = operators.F;
+Bhat = operators.B;
 
 for j = 1:length(r_vals)
     r = r_vals(j);
     Vr = U_svd(:,1:r);
     
-    [operators] = inferOperators(X, U, Vr, params, R);
-    Ahat = operators.A;
-    Fhat = operators.F;
-    Bhat = operators.B;
-    
-    % MODIFIED %%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    Vr = U_svd(:,1:r);
-    Aint = Vr' * A * Vr;
-    Bint = Vr' * B;
-    Ln = elimat(N); Dr = dupmat(r);
-    Fint = Vr' * F * Ln * kron(Vr,Vr) * Dr;
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-    s_hat = semiImplicitEuler(Ahat,Fhat,Bhat,dt,u_ref);
-    s_rec = Vr(:,1:r)*s_hat;
+    Fhat_extract = extractF(Fhat, r);
+    s_hat = semiImplicitEuler(Ahat(1:r,1:r),Fhat_extract,Bhat(1:r,:),dt,u_ref);
+    s_rec = Vr*s_hat;
     err_inf(j) = norm(s_rec-s_ref,'fro')/norm(s_ref,'fro');
     
-    % MODIFIED %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    s_int = semiImplicitEuler(Aint,Fint,Bint,dt,u_ref);
+    Fint_extract = extractF(Fint, r);
+    s_int = semiImplicitEuler(Aint(1:r,1:r),Fint_extract,Bint(1:r,:),dt,u_ref);
     s_tmp = Vr*s_int;
     err_int(j) = norm(s_tmp-s_ref,'fro')/norm(s_ref,'fro');
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 end
 
-% MODIFIED %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 figure(2); clf
 semilogy(r_vals,err_inf, DisplayName="opinf"); grid on; grid minor; hold on;
 semilogy(r_vals,err_int, DisplayName="int"); 
@@ -92,47 +99,46 @@ hold off; legend(Location="southwest");
 xlabel('Model size $r$','Interpreter','LaTeX')
 ylabel('Relative state reconstruction error','Interpreter','LaTeX')
 title('Burgers inferred model error','Interpreter','LaTeX')
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 
 %% semi-implicit Euler scheme for integrating learned model from zero initial condition
 function s_hat = semiImplicitEuler(Ahat, Fhat, Bhat, dt, u_input)
-K = size(u_input,1);
-r = size(Ahat,1);
-s_hat = zeros(r,K+1); % initial state is zeros everywhere
-ImdtA = eye(r) - dt*Ahat;
-for i = 1:K
-    ssq = get_x_sq(s_hat(:,i)')';
-    s_hat(:,i+1) = ImdtA\(s_hat(:,i) + dt*Fhat*ssq + dt*Bhat*u_input(i));
-    if any(isnan(s_hat(:,i+1)))
-        warning(['ROM unstable at ',num2str(i),'th timestep'])
-        break
+    K = size(u_input,1);
+    r = size(Ahat,1);
+    s_hat = zeros(r,K+1); % initial state is zeros everywhere
+    ImdtA = eye(r) - dt*Ahat;
+    for i = 1:K
+        ssq = get_x_sq(s_hat(:,i)')';
+        s_hat(:,i+1) = ImdtA\(s_hat(:,i) + dt*Fhat*ssq + dt*Bhat*u_input(i));
+        if any(isnan(s_hat(:,i+1)))
+            warning(['ROM unstable at ',num2str(i),'th timestep'])
+            break
+        end
     end
-end
 end
 
 %% solves Burgers equation from zero initial condition with specified input
-function [s_all,A,B,F] = burgersFOM(N,dt,T_end,mu,u)
-dx = 1/(N-1);
-
-K = T_end/dt;
-
-[A,B,F] = getBurgersMatrices(N,dx,mu);
-ImdtA = eye(N)-dt*A;
-% ImdtA(1,1:2) = [1 0]; ImdtA(N,N-1:N) = [0 1]; % Dirichlet boundary conditions
-
-s_all = zeros(N,K+1);       % initial state is zero everywhere
-for i = 1:K
-    ssq = get_x_sq(s_all(:,i)')';
-    s_all(:,i+1) = ImdtA\([0; s_all(2:N-1,i); 0] - dt*F*ssq + dt*B*u(i));
-end
+function [s_all,A,B,F,ImdtA] = burgersFOM(N,dt,T_end,mu,u)
+    dx = 1/(N-1);
+    
+    K = T_end/dt;
+    
+    [A,B,F] = getBurgersMatrices(N,dx,dt,mu);
+    ImdtA = eye(N)-dt*A;
+    % ImdtA(1,1:2) = [1 0]; ImdtA(N,N-1:N) = [0 1]; % Dirichlet boundary conditions
+    
+    s_all = zeros(N,K+1);       % initial state is zero everywhere
+    for i = 1:K
+        ssq = get_x_sq(s_all(:,i)')';
+        s_all(:,i+1) = ImdtA\([0; s_all(2:N-1,i); 0] + dt*F*ssq + dt*B*u(i));
+    end
 end
 
 %% builds matrices for Burgers full-order model
-function [A, B, F] = getBurgersMatrices(N,dx,mu)
+function [A, B, F] = getBurgersMatrices(N,dx,dt,mu)
     % construct linear operator resulting from second derivative
     A = mu*gallery('tridiag',N,1,-2,1)/(dx^2);
-    A(1,1:2) = [1 0]; A(N,N-1:N) = [0 1]; % Dirichlet boundary conditions
+    A(1,1:2) = [0 0]; A(N,N-1:N) = [0 0]; % Dirichlet boundary conditions
 
     % construct quadratic operator F using central difference for the
     % derivative
@@ -144,8 +150,8 @@ function [A, B, F] = getBurgersMatrices(N,dx,mu)
     jm = mm + 1;        % this is the index of the x_{i-1}*x_i term
     jj = reshape([jp; jm],2*N-4,1);
     vv = reshape([ones(1,N-2); -ones(1,N-2)],2*N-4,1)/(2*dx);
-    F = sparse(ii,jj,vv,N,N*(N+1)/2);
+    F = -sparse(ii,jj,vv,N,N*(N+1)/2);
 
     % construct input matrix B
-    B = [1; zeros(N-2,1); -1];
+    B = [1; zeros(N-2,1); -1]/dt;
 end
